28d0d8024cc7892775c21bc26abc3c2556e0f281
AlderAutomation <mheltman@alderautomation.ca>
2023-02-08 15:01:09 -0800

    Setup logger into game directory

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-import logging 
-
-LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(filename="log.log", level=logging.DEBUG, format = LOG_FORMAT)
-mylog = logging.getLogger()
+from kivy.logger import Logger
+from kivy.config import Config
 
 kivy.require("1.9.1")
-win_bg_color = 1,1,1,1
+
+Config.set('kivy', 'log_dir', '/home/picard/tamagotchi/logs/')
+Config.set('kivy', 'log_level', 'debug')
+Config.write()
 
 
 class Start(BoxLayout):
     def new_game(self):
+        Logger.debug("Application: New Game Button pressed \n")
         self.clear_widgets()
         self.add_widget(NewGame()) 
 
 
     def load_game(self):
-        print("continue")
+        Logger.debug("Application: Load Game Button pressed \n")
 
 
     def options(self):
-        print("options")
+        Logger.debug("Application: Options Button pressed \n")
 
 
 class NewGame(BoxLayout):
-    print("New Game Button Pushed")
+    pass
 
 
 
